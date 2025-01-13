cc9b26d464ff95f171863ce58c6d152b3ffc7e1b
Filip Zdebik <filip.zdebik@gmail.com>
2025-01-13 19:10:28 +0100

    refactor code in backend move some functionality to token handler, still working on adding users to the rooms with proper validation

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
-const {Server} = require("socket.io")
+import { Server } from "socket.io";
+import { getUserByToken,addUser,logUsers,removeSocketIdByToken,addSocketIdByToken} from "./token_handler.js";
 const io = new Server(8080, {
     cors: {
         origin: 'http://127.0.0.1:5500',
@@ -6,73 +7,89 @@ const io = new Server(8080, {
     },
 });
 console.log("Websocket running on port 8080")
-const generateSessionToken = () => {
-    return Math.random().toString(36).substr(2); // Random token
-};
+
+const validateRoomJoin = (roomName,token,username,socketId)=>{
+    const responseObj={status:0,message:"succesfull"}
+    if(!roomData[roomName]){
+        responseObj.status=1
+        responseObj.message = "room with given name not found"
+        return responseObj
+    }
+    console.log(token);
+    console.log(token===undefined);
+    
+    
+    if(!token == undefined){
+        
+        const user = getUserByToken(token)
+        if(!user){
+            responseObj.status = 2
+            responseObj.message = "user with given token not found"
+            return responseObj
+        }else if(user.socketId!==socketId){
+            responseObj.status=4
+            responseObj.message = "user with given token already in game"
+            return responseObj
+        }
+    }else{
+        roomData[roomName].users.forEach(user=>{
+            const foundUser = getUserByToken(user.token)
+            if(foundUser.username===username){
+                responseObj.status=3
+                responseObj.message = "user with that username already in game"
+                return responseObj
+            }
+        })
+    }
+
+    return responseObj
+
+}
+const onRoomJoin = (socket,roomName,username)=>{
+    const token = addUser(username,roomName,socket.id)
+            
+    roomData[roomName].users.push({id:socket.id,token:token})
+    socket.to(roomName).emit('newUser',roomData[roomName])
+    socket.emit("roomJoinResponse",{join_status:200,message:"succesfully joined a room",token:token})
+    return token
+
+}
 const roomData = {
     "num":{admin:{},users:[]}
 }
-const users = []
+
 io.on('connection', (socket)=>{
-    const token = socket.handshake.query.token
-    if(token){
-        var connected = false
-        users.forEach(user=>{
-            if(user[0]===token){
-                socket.join(user[1])
-                connected=true
-                return
-            }
-        })
-        if(!connected){
-            socket.emit("tokenValidation",{token_status:"invalid"})
-        }else{
+    var token = socket.handshake.query.token  
+    if(token!==null){
+        console.log(token);
+        const user = getUserByToken(token)
+        if(user){
             socket.emit("tokenValidation",{token_status:"valid"})
+            addSocketIdByToken(token,socket.id)
+            logUsers()
+        }else{
+            socket.emit("tokenValidation",{token_status:"invalid"})
         }
     }
-    //Object.keys(roomData).forEach(roomName=>{
-    //    roomData[roomName].users.forEach(user=>{
-    //        if(user.token===token){
-    //            socket.join(roomName)
-    //        }
-    //    })
-    //})
+    
     
     console.log(`Client connected: ${socket.id}`)
 
     socket.on("joinRoom",(roomName,username,sessionToken)=>{
-        if(sessionToken){
-            var connected = false
-            console.log(users);
-            
-            users.forEach(user=>{
-                if(user[0]===token){
-                    socket.join(user[1])
-                    connected=true
-                    return
-                }
-            })
-            if(connected){
-                return
-            }
-        }
-        if(!roomData[roomName]){
-            socket.emit("roomJoinResponse",{join_status:404,message:"Room not found"})
-        }
-        else{
+        console.log(sessionToken);
+        logUsers()
+        const validation = validateRoomJoin(roomName,sessionToken,username)
+        console.log(validation);
+        
+        if(validation.status===0){
             socket.join(roomName)
-            const token = generateSessionToken()
-            console.log(token);
-            users.push([token,roomName])
-            
-            roomData[roomName].users.push({id:socket.id,token:token,name:username})
-            console.log(`User ${socket.id} joined room ${roomName}`)
-            socket.to(roomName).emit('newUser',roomData[roomName])
-            socket.emit("roomJoinResponse",{join_status:200,message:"succesfully joined a room",token:token})
+            token = onRoomJoin(socket,roomName,username)
+        }else{
+            socket.emit("roomJoinResponse",{join_status:validation.status,message:validation.message,token:token})
         }
     })
 
     socket.on('disconnect',()=>{
-
+        removeSocketIdByToken(token)
     })
 })
--- frontend/index.js
+++ frontend/index.js
@@ -1,23 +1,35 @@
-const socket = io("ws://localhost:8080")
-
 const buttonJoin = document.getElementById("join")
 const codeInput =document.getElementById("code")
 const nameInput = document.getElementById("username")
 var joined = false
-
 function getSessionTokenFromCookies() {
     const cookies = document.cookie.split("; ");
     for (let cookie of cookies) {
         if (cookie.startsWith("session_token=")) {
-            return cookie.split("=")[1];
+            const token = cookie.split("=")[1]
+            if(token!=="")
+            return token;
+            else
+            return null
         }
     }
     return null;
 }
-const sessionToken = getSessionTokenFromCookies() 
+const sessionToken = getSessionTokenFromCookies()
+console.log(`session token: ${sessionToken}`)
 
+const socket = io("ws://localhost:8080",{
+    query:{
+        token:sessionToken
+    }
+    }
+)
+
+socket.once("tokenValidation",(data)=>{
+    console.log(data.token_status)
+})
 const onJoin = ()=>{
-    window.location="/frontend/waitroom.html"
+    //window.location="/frontend/waitroom.html"
 }
 
 buttonJoin.addEventListener("click",()=>{
