607bbe0ccdef1ecd1d1f5e61df7eae6c7bed0531
Filip Zdebik <filip.zdebik@gmail.com>
2025-01-24 21:18:12 +0100

    make a homepage UI more appearing and add inputs for creating a room

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,20 @@
-const buttonJoin = document.getElementById("join")
-const buttonCreate = document.getElementById("create")
-const codeInput =document.getElementById("code")
-const nameInput = document.getElementById("username")
+const MIN_NUM_OF_PLAYERS = 5
+const MIN_NUM_OF_MAFIA = 1
+const MAX_NUM_OF_PLAYERS = 40
+const MAX_NUM_OF_MAFIA = 8
+var buttonJoin = document.querySelector("div#form #join")
+var buttonCreate = document.querySelector("div#form #create")
+var codeInput =document.getElementById("code")
+var nameInput = document.getElementById("username")
+var numMafInput = document.querySelector("input#numOfMaf")
+var numPlayInput = document.querySelector("input#numOfPlay")
+var numMafLabel = document.querySelector("label#numOfMaf")
+var numPlayLabel = document.querySelector("label#numOfPlay")
+const form = document.querySelector("#form")
+const buttonFormTypeJoin = document.querySelector("div#pickType #join")
+const buttonFormTypeCreate = document.querySelector("div#pickType #create")
 
-function getSessionTokenFromCookies() {
+const getSessionTokenFromCookies = ()=> {
     const cookies = document.cookie.split("; ");
     for (let cookie of cookies) {
         if (cookie.startsWith("session_token=")) {
@@ -17,8 +28,6 @@ function getSessionTokenFromCookies() {
     return ""
 }
 var sessionToken = getSessionTokenFromCookies()
-console.log(`session token: ${sessionToken}`)
-
 const socket = io("ws://localhost:8080",{
     query:{
         token:sessionToken
@@ -26,15 +35,71 @@ const socket = io("ws://localhost:8080",{
     }
 )
 
-const onJoin = (roomName,username)=>{
-    sessionStorage.setItem("roomName",roomName)
-    sessionStorage.setItem("username",username)
-
-    sessionToken = getSessionTokenFromCookies()
-    window.location="/frontend/waitroom.html"
+const main = ()=>{
+    addListenersToJoinForm()
+    buttonFormTypeCreate.addEventListener("click",loadCreateForm)
+    buttonFormTypeJoin.addEventListener("click",loadJoinForm)
 }
 
-buttonJoin.addEventListener("click",()=>{
+const loadCreateForm = ()=>{
+    buttonFormTypeCreate.classList.add("picked")
+    buttonFormTypeJoin.classList.remove("picked")
+    buttonJoin.removeEventListener("click",handleButtonJoinClick)
+    form.innerHTML = `
+            <h1>MAFIA GAME</h1>
+            <label>code</label>
+            <input type="text" id="code"/>
+            <label>username</label>
+            <input type="text" id="username"/>
+            <label>Maximum number of players</label>
+            <label id="numOfPlay">5</label>
+            <input type="range" id="numOfPlay" value="1"/> 
+            <label>Number of mafias</label>
+            <label id="numOfMaf">1</label>
+            <input type="range" id="numOfMaf" value="1"/> 
+            <button id="create">CREATE</button>
+    ` 
+    buttonCreate = document.querySelector("div#form #create")
+    codeInput =document.getElementById("code")
+    nameInput = document.getElementById("username")
+    numMafInput = document.querySelector("input#numOfMaf")
+    numPlayInput = document.querySelector("input#numOfPlay")
+    numMafLabel = document.querySelector("label#numOfMaf")
+    numPlayLabel = document.querySelector("label#numOfPlay")    
+    addListenersToCreateForm()
+}
+const loadJoinForm = ()=>{
+    buttonFormTypeJoin.classList.add("picked")
+    buttonFormTypeCreate.classList.remove("picked")
+    buttonCreate.removeEventListener("click",handleButtonCreateClick)
+    form.innerHTML = `
+            <h1>MAFIA GAME</h1>
+            <label>code</label>
+            <input type="text" id="code"/>
+            <label>username</label>
+            <input type="text" id="username"/>
+            <button id="join">JOIN</button>
+    ` 
+    buttonJoin = document.querySelector("div#form #join")
+    codeInput =document.getElementById("code")
+    nameInput = document.getElementById("username")
+    addListenersToJoinForm()
+}
+const addListenersToCreateForm = ()=>{
+    buttonCreate.addEventListener("click",handleButtonCreateClick)
+
+    numMafInput.addEventListener("change",(e)=>{
+        numMafLabel.innerHTML = MIN_NUM_OF_MAFIA+Math.max(0,Math.round(e.target.value*(MAX_NUM_OF_MAFIA-MIN_NUM_OF_MAFIA)/100))
+    })
+    numPlayInput.addEventListener("change",(e)=>{
+        numPlayLabel.innerHTML = MIN_NUM_OF_PLAYERS+Math.max(0,Math.round(e.target.value*(MAX_NUM_OF_PLAYERS-MIN_NUM_OF_PLAYERS)/100))
+    })
+}
+const addListenersToJoinForm = ()=>{
+    buttonJoin.addEventListener("click",handleButtonJoinClick)
+}
+
+const handleButtonJoinClick = ()=>{
     var roomName = codeInput.value
     var username = nameInput.value
     if(!roomName){
@@ -60,18 +125,24 @@ buttonJoin.addEventListener("click",()=>{
         console.log(obj.message);
     
     })
-})
-
-buttonCreate.addEventListener("click",()=>{
+}
+const handleButtonCreateClick = ()=>{
     var roomName = codeInput.value
     var username = nameInput.value
-    if(!roomName){
+    var maxNumPlay = MIN_NUM_OF_PLAYERS+Math.max(0,Math.round(numMafInput.value*(MAX_NUM_OF_MAFIA-MIN_NUM_OF_MAFIA)/100))
+    var numOfMaf = MIN_NUM_OF_MAFIA+Math.max(0,Math.round(numPlayInput.value*(MAX_NUM_OF_PLAYERS-MIN_NUM_OF_PLAYERS)/100))
+    if(!roomName || !username || !maxNumPlay || !numOfMaf){
         alert("You must fill a code input")
         return
     }
-    if(!username){
-        alert("You must fill a username input")
-        return
+    if(maxNumPlay>MAX_NUM_OF_PLAYERS || numOfMaf > MAX_NUM_OF_MAFIA){
+        alert("Too big of a value in the number of mafias or max number of players")
+    }
+    const createRoomObj = {
+        roomName:roomName,
+        username:username,
+        maxNumPlay:maxNumPlay,
+        numOfMaf:numOfMaf
     }
 
     socket.emit('createRoom',roomName,username,sessionToken)
@@ -88,4 +159,12 @@ buttonCreate.addEventListener("click",()=>{
         console.log(obj.message);
     
     })
-})
+}
+const onJoin = (roomName,username)=>{
+    sessionStorage.setItem("roomName",roomName)
+    sessionStorage.setItem("username",username)
+
+    sessionToken = getSessionTokenFromCookies()
+    window.location="/frontend/waitroom.html"
+}
+main()
